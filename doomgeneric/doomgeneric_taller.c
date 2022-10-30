e4b84810861664ac97f152ba707205e51eb7ed37
Alejandro Soto <alejandro@34project.org>
2022-10-29 18:52:03 -0600

    Add target: taller

---
+++ doomgeneric/doomgeneric_taller.c
@@ -0,0 +1,33 @@
+#include "doomkeys.h"
+#include "doomgeneric.h"
+#include "m_argv.h"
+
+void DG_Init() {}
+
+void DG_DrawFrame()
+{
+	//TODO
+	//DG_ScreenBuffer
+}
+
+void DG_SleepMs(uint32_t ms)
+{
+	//TODO
+}
+
+uint32_t DG_GetTicksMs()
+{
+	//TODO
+	return 0;
+}
+
+int DG_GetKey(int *pressed, unsigned char *doomKey)
+{
+	//TODO
+	return 0;
+}
+
+void DG_SetWindowTitle(const char *title)
+{
+	(void)title;
+}
